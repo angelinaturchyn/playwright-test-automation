5707f2cdb094c84cf8f676863fcb3fe22648312a
Angelin Turchyn <aturchyn@100MBP-ATURCHYN.local>
2022-04-02 23:14:04 -0500

    add url

--- tests/fox.spec.js
+++ tests/fox.spec.js
@@ -2,6 +2,7 @@ const { test, expect } = require('@playwright/test');
 
 test.beforeEach(async ({ page }) => {
   // Go to the starting url before each test.
+  await page.goto('https:/foxtrotco.com');
   await expect(page).toHaveURL('https:/foxtrotco.com');
 });
 
@@ -12,7 +13,7 @@ test('basic test', async ({ page }) => {
   await expect(page.locator('text=Our Stores').first()).toBeVisible()
   await page.click('//span[text()="Dallas"]')
 });
-test('basic test', async ({ page }) => {
+test('second test', async ({ page }) => {
   await expect(page).toHaveURL('https:/foxtrotco.com');
   await expect(page).toHaveTitle('Foxtrot');
   await expect(page.locator('//parent::span//a[@kind="primary"]').first()).toHaveAttribute('href', '/stores')
