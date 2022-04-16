23373de560eab99448483ffc3ff143f0ec98b9b7
Angelin Turchyn <aturchyn@100MBP-ATURCHYN.local>
2022-04-16 15:12:03 -0500

    added dropdown

--- tests/afterHook.spec.js
+++ tests/afterHook.spec.js
@@ -2,25 +2,28 @@ const { test, expect } = require('@playwright/test');
 
 test.beforeEach(async ({ page }) => {
   // Go to the starting url before each test.
+  await page.goto('https:/foxtrotco.com');
   await expect(page).toHaveURL('https:/foxtrotco.com');
 });
 
-test('basic test', async ({ page }) => {
+test.only('Dropdown State', async ({ page }) => {
+  
   await expect(page).toHaveTitle('Foxtrot');
-  await expect(page.locator('//parent::span//a[@kind="primary"]').first()).toHaveAttribute('href', '/stores')
-  await page.click('//parent::span//a[@kind="primary"]')
-  await expect(page.locator('text=Our Stores').first()).toBeVisible()
-  await page.click('//span[text()="Dallas"]')
-});
-test('basic test', async ({ page }) => {
-  await expect(page).toHaveURL('https:/foxtrotco.com');
-  await expect(page).toHaveTitle('Foxtrot');
-  await expect(page.locator('//parent::span//a[@kind="primary"]').first()).toHaveAttribute('href', '/stores')
-  await page.click('//parent::span//a[@kind="primary"]')
-  await expect(page.locator('text=Our Stores').first()).toBeVisible()
-  await page.click('//span[text()="Chicago"]')
-});
+  
+  await page.click('[data-testid="justBrowseLink"]')
+  await expect(page.locator('[class*="styled__MenuListItem"]').first()).toBeVisible()
+  await page.locator('[class*="styled__MenuListItem"]').first().click()
+  await page.locator('[class*="ButtonContentContainer"]').click()
+  await page.locator('[class*="ToggleButton"]:nth-of-type(2)').click()
+  const dropdown = page.locator('select[class*="RegionSelector"]')
+  await dropdown.click()
+  await dropdown.selectOption('4')
+  await page.waitForTimeout(2000)
+  const region = page.locator('[class*="styled__AddressList"]').first()
+  await page.waitForTimeout(2000)
+  await region.click()
 
+});
 
 test.afterEach(async ({ page }, testInfo) => {
   console.log(`Finished ${testInfo.title} with status ${testInfo.status}`);
