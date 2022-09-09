8a903f7a6f9e77fd9c1878184646b3af05399557
angelinaturchyn <75140309+angelinaturchyn@users.noreply.github.com>
2022-09-08 22:35:33 -0500

    Update saveSessionStorage.spec.js

--- tests/saveSessionStorage.spec.js
+++ tests/saveSessionStorage.spec.js
@@ -4,12 +4,12 @@ test.only('Log In through UI on staging gating page', async ({ browser }) => {
  const context = await browser.newContext();  
  const page = await context.newPage(); 
  await page.goto('https:/staging.foxtrotco.com');
- await page.locator('#email').fill('.com')
- await page.locator('#password').fill(' @')
+ await page.locator('#email').fill('awesome@gmail.com')
+ await page.locator('#password').fill('1223@')
  await page.locator('button[type="submit"]').click()
  await page.waitForNavigation()
  await page.locator('text="Log in"').click()
- await page.locator('#email').fill('a.com')
+ await page.locator('#email').fill('a33.com')
  await page.locator('#password').fill('1234567')
  await page.locator('button[class*="TangoButton"]').first().click()
  await context.storageState({path: 'session.json'})
