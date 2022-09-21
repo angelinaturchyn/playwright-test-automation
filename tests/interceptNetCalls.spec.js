73cc8285732fdbd13a28690fde7b183333c7d832
Angelin Turchyn <aturchyn@100MBP-ATURCHYN.local>
2022-09-21 00:09:42 -0500

    added login to backbone

--- tests/interceptNetCalls.spec.js
+++ tests/interceptNetCalls.spec.js
@@ -1,27 +1,36 @@
 const {test, exepect, request} = require('@playwright/test');
 const { APIUtils } = require('./utils/apiUtils');
+const backboneLogin = {username: "aturchyn@foxtrotco.com", password: "Angelina10trinity@"}
 const loginPayload = {email: "loop@gmail.com", password: "1234567"};
 const createCartPayload = {cart_type: "standard", on_conflict: "merge", order_type: "pickup", store_id: 2};
-const fakePayLoadOrders = {"status":1,"orders":[]}
-
+const fakePayLoadOrders = {status:1,orders:[]}
+let token
 
 
 test.only('Login as an auth user', async ({page}) => {
    const apiContext = await request.newContext();
-   const apiUtils = new APIUtils(apiContext,loginPayload)
-   let token = await apiUtils.getToken()
+   const apiUtils = new APIUtils(apiContext,backboneLogin,loginPayload)
+   await apiUtils.loginBackbone()
+   token = await apiUtils.getToken()
 
-   await page.route(`https://staging.api.foxtrotchicago.com/v5/users/order-history?`,
-   async route => {
-      const response = await page.request.fetch(route.request())
-      let body = fakePayLoadOrders
-      route.fulfill(
-        {
-            response,
-            body,
-        }
-      )
-   })
-    await page.goto('https://staging.foxtrotco.com/account/order-history')
+  //  page.addInitScript(value => {
+  //   window.localStorage.setItem('token', value);
+  // }, token)
+  
+   await page.goto(' https://foxtrot-next-fe-sandbox.onrender.com/home')
+   page.pause()
 
+
+  //  await page.route(`https://staging.api.foxtrotchicago.com/v5/users/order-history?`,
+  //  async route => {
+  //     const response = await page.request.fetch(route.request())
+  //     let body = fakePayLoadOrders
+  //     route.fulfill(
+  //       {
+  //           response,
+  //           body
+  //       }
+  //     )
+  //  })
+   
 })
